676cf94ccf2bbbacb14f2f780da3b5af5630d5f2
EC2 Default User <ec2-user@ip-172-31-50-51.ec2.internal>
2021-01-06 05:13:14 +0000

    create sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1b4c8906f6b70ba69b762a33c4103a8509452b6492201f8b3878b092817e9515f141a4363d7104b3af85d9624d32655b6ffd55094150200c17c6a096813ebbc1'
+
---
+++ db/migrate/20210105090727_remove_name_to_lists.rb
@@ -0,0 +1,4 @@
+class RemoveNameToLists < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
